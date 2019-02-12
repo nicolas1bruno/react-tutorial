6b989c9c0f963c9fcca02e5c2d8371da973b9ae6
Nicolas Bruno <nicolas.bruno@ibm.com>
2019-02-12 15:07:39 -0200

    Updates

---
+++ precache-manifest.dc5e6ab509169d8a579d81b33e9c510c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9b2d15347dbf2dd6be24",
+    "url": "/react-tutorial/static/js/runtime~main.9b2d1534.js"
+  },
+  {
+    "revision": "24ae0f359d47b78b440b",
+    "url": "/react-tutorial/static/js/main.24ae0f35.chunk.js"
+  },
+  {
+    "revision": "1079982f45043f536abc",
+    "url": "/react-tutorial/static/js/2.1079982f.chunk.js"
+  },
+  {
+    "revision": "24ae0f359d47b78b440b",
+    "url": "/react-tutorial/static/css/main.86450440.chunk.css"
+  },
+  {
+    "revision": "34579c7272d155df0a0afbd5a3a988bd",
+    "url": "/react-tutorial/index.html"
+  }
+];
